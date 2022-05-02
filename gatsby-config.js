02c84144545e77043d97ebcbf1cb8238a3d8760d
Calil Drissi <calil.drissi@gmail.com>
2022-05-02 08:41:44 +0000

    deploying

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,15 @@ module.exports = {
   },
   plugins: [`gatsby-plugin-styled-components`,
   {
-    resolve: `gatsby-plugin-prefetch-google-fonts`,
+    resolve: `gatsby-plugin-google-fonts-v2`,
     options: {
       fonts: [
         {
-          family: `Kanit`,
-          variants:  [ `100`,`200`,`400`,`500`, `600`, `700`, `800`, `900`]
-        },
-      ],
-    },
-  }
+          family: `Kanit:wght@100,200,300;400;500;600;700,800`,
+        }
+      
+      ]
+    }
+  },
 ],
 }
